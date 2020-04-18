e987c5afd6e0ebead32766e5fb94d008de5e6363
Abdyrasul <abdyrasul2015@gmail.com>
2020-04-18 20:47:26 +0300

    Navbar design(Not completed)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,46 @@
 import React, { Component } from "react";
 import "../style/style.css";
+// import "font-awesome/css/font-awesome.min.css";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export class Navbar extends Component {
+  // const element = <FontAwesomeIcon icon={faCoffee} />
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="container">
             <h1 className="logo">ABDYRASUL</h1>
-            <ul className="nav">
-              <li>
-                <a href="#home">Home</a>
-              </li>
-              <li>
-                <a href="#home">Blog</a>
-              </li>
-              <li>
-                <a href="#home">Photography</a>
-              </li>
-              <li>
-                <a href="#home">About</a>
-              </li>
-              <li>
-                <a href="#home">Contact</a>
-              </li>
-              <li>
-                <a href="#home">Draw</a>
-              </li>
-            </ul>
+            <div className="mainNavbar">
+              <ul className="nav">
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#home">Blog</a>
+                </li>
+                <li>
+                  <a href="#home">Photography</a>
+                </li>
+                <li>
+                  <a href="#home">Portfolio</a>
+                </li>
+              </ul>
+              <ul className="rightMenu">
+                <li>
+                  <a href="#">
+                    <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </div>
